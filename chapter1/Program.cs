e47fd862247346e26c2f852f14cdded8248d5e8e
lapeko <lapeko88@gmail.com>
2023-10-02 18:07:14 +0200

    start learning c#

---
+++ chapter1/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("Press Enter to exit...");
+Console.Read();
---
+++ chapter2-basics/1-types.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Channels;
+
+int x = -1;
+
+int y;
+y = 1;
+
+// Int32 x1 = -1;
+// uint x2 = 32;
+
+float f = 1.1F;
+
+// next two lines are equivalent
+int x5 = 0;
+int x6 = new int();
+
+// int this case - int a = 1 will be the same
+var a = 1;
+
+// using var there can be convenient
+Dictionary<int, string> dict = new Dictionary<int, string>();
+var dict2 = new Dictionary<int, string>();
+
+decimal dc = 3.1m;
+
+char character = 'a';
+string name = "Vitali";
+
+bool canDrive = true;
+
+object obj = 1;
+object obj2 = "123";
+object @object = 'c';
+
+Console.WriteLine();
