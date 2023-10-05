71fa9cb2d9af7d2edcef5eca94523bc05a2420a6
lapeko <lapeko88@gmail.com>
2023-10-05 06:12:45 +0200

    homework2

--- chapter3-control-flow/Program.cs
+++ chapter3-control-flow/Program.cs
@@ -1,4 +1,36 @@
-﻿RunFibonacci();
+﻿Run10Inputs();
+
+void Run10Inputs()
+{
+    var size = 0;
+    var userNumbers = new int[10];
+
+    do
+    {
+        Console.WriteLine($"Enter positive number{size + 1}:");
+        var input = Console.ReadLine();
+        if (input == null) return;
+        var newNumber = int.Parse(input);
+        if (newNumber < 0) continue;
+        if (newNumber == 0) break;
+        userNumbers[size++] = newNumber;
+    } while (size < 10);
+
+    Console.WriteLine("You entered:");
+    for (var i = 0; i < size; i++)
+        Console.WriteLine($"{i + 1}: {userNumbers[i]}");
+
+    double sum = 0;
+    var count = 0;
+    for (var i = 0; i < size; i++)
+    {
+        if (userNumbers[i] % 3 != 0) continue;
+        sum += userNumbers[i];
+        count++;
+    }
+        
+    Console.WriteLine($"Average of positive integers that are multiples of three is {(sum / count):F}");
+}
 
 void RunFibonacci()
 {
