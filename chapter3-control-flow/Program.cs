b8a08c6515fcf6b91d03b0bf804f96b50547596f
lapeko <lapeko88@gmail.com>
2023-10-05 05:52:08 +0200

    start chapter3. homework1 - fibonacci

---
+++ chapter3-control-flow/Program.cs
@@ -0,0 +1,19 @@
+﻿RunFibonacci();
+
+void RunFibonacci()
+{
+    Console.WriteLine("Write size number of fibonacci: ");
+    var input = Console.ReadLine();
+    if (input == null) return;
+    var size = int.Parse(input);
+
+    var fibonacciArr = new int[size];
+    fibonacciArr[0] = 1;
+    fibonacciArr[1] = 1;
+    
+    for (int i = 2; i < size; i++)
+        fibonacciArr[i] = fibonacciArr[i - 2] + fibonacciArr[i - 1];
+
+    Console.WriteLine("output:");
+    for (int i = 0; i < size; i++) Console.WriteLine(fibonacciArr[i]);
+}
