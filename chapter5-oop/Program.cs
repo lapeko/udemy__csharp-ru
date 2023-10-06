0b5320c2c94636288f41119da3d1d1b21fbc7826
lapeko <lapeko88@gmail.com>
2023-10-07 01:14:25 +0200

    learn trydo

--- chapter5-oop/Program.cs
+++ chapter5-oop/Program.cs
@@ -1,9 +1,13 @@
 ﻿using System.Threading.Channels;
 using chapter5_oop;
+using chapter5_oop.obj;
 
 // var res = RomeNumbers.ConvertToArabic("XIV");
 // Console.WriteLine(res);
 
-Console.WriteLine(AreaCalculator.CalcForTriangle(3, 4, 5D));
-Console.WriteLine(AreaCalculator.CalcForTriangle(3, 4));
-Console.WriteLine(AreaCalculator.CalcForTriangle(3, 4, 90));
+// Console.WriteLine(AreaCalculator.CalcForTriangle(3, 4, 5D));
+// Console.WriteLine(AreaCalculator.CalcForTriangle(3, 4));
+// Console.WriteLine(AreaCalculator.CalcForTriangle(3, 4, 90));
+
+TryDo.TryDivide(1, 2, out double result);
+Console.WriteLine(result);
