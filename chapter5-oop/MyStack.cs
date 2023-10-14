1b2b0d150dee4c26f2aac0bbbd94c09b6a532859
lapeko <lapeko88@gmail.com>
2023-10-14 14:50:00 +0200

    refactor MyStack. Create two constructors

--- chapter5-oop/MyStack.cs
+++ chapter5-oop/MyStack.cs
@@ -1,6 +1,6 @@
 namespace chapter5_oop;
 
-interface IMyStack
+internal interface IMyStack
 {
     int Count { get; }
     void Push(object item);
@@ -10,31 +10,41 @@ interface IMyStack
 
 public class MyStack: IMyStack
 {
-    public int Count { get; private set; } = 0;
-    private object[] _stack = new object[4];
+    public int Count { get; private set; }
+    private object[] _items;
+
+    public MyStack()
+    {
+        _items = new object[4];
+    }
+
+    public MyStack(uint capacity)
+    {
+        _items = new object[capacity];
+    }
 
     public void Push(object item)
     {
-        if (Count == _stack.Length)
+        if (Count == _items.Length)
         {
-            var newLength = _stack.Length * 2;
-            var newStack = new object[newLength];
-            _stack.CopyTo(newStack, 0);
-            _stack = newStack;
+            var newStack = new object[_items.Length * 2];
+            _items.CopyTo(newStack, 0);
+            _items = newStack;
         }
-        
-        _stack[Count++] = item;
+        _items[Count++] = item;
     }
 
     public object Pop()
     {
         if (Count == 0) throw new InvalidOperationException("Stack is empty");
-        return _stack[--Count];
+        
+        return _items[--Count];
     }
 
     public object Peek()
     {
         if (Count == 0) throw new InvalidOperationException("Stack is empty");
-        return _stack[Count - 1];
+        
+        return _items[Count - 1];
     }
 }
